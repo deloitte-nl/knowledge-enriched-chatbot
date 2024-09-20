f1de4a888b9ea4da874d3e1702f61f40375eccb1
Emma Machielse <33420082+emsalinha@users.noreply.github.com>
2024-09-20 14:46:23 +0200

    Add license (#2)
    * add license
    * add license references in src files
    * refer to own License MD file

--- src/RAG/document_loader.py
+++ src/RAG/document_loader.py
@@ -1,3 +1,20 @@
+# Knowledge-enriched chatbot
+# Copyright (C) 2024 Deloitte Risk Advisory B.V. 
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You have received a copy of the GNU Affero General Public License
+# and our supplemental terms in LICENSE.MD in the root folder.  
+
+
 import copy
 from collections import OrderedDict
 from pathlib import Path
